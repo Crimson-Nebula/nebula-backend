386cbad54daa8f5983a2be25b0ce87847ea20e99
KimJammer <41841812+kimjammer@users.noreply.github.com>
2024-11-01 23:36:26 -0400

    Retry connecting to Postgres DB while it launches

--- nebula/__init__.py
+++ nebula/__init__.py
@@ -17,29 +17,19 @@ def create_app(test_config=None):
     CORS(app, supports_credentials=True)
 
     # Connecting to the database
-    # Creating a instance of the class to connect, 
+    # Creating an instance of the class to connect,
     # Here the instance of the connection is made directly as it is under the __init__ function of the class
-    init_db()
     app.config['DB_CONNECTION'] = Database()
-    db = Database()
-
 
     # a simple page that says hello
     @app.route('/')
     def hello():
         return jsonify({"Hello": "World"})
-
     
     app.register_blueprint(user.bp)
     app.register_blueprint(post.bp)
     app.register_blueprint(feed.bp)
 
-    # new_doc1 = {"username": "John Doe", "auth_id": "54", "user_id": "56"}
-    # new_doc2 = {"username": "John Doe", "auth_id": "45", "user_id": "54"}
-    # db.create_user(new_doc1)
-    # db.create_user(new_doc2)
-    # test_related = db.get_user_by_auth_id("45")
-    # print(test_related[0].user_id)
     return app
 
 
